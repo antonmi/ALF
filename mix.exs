8b9ac10c34c1ca50383e9eefae867a62606aa5b9
Anton Mishchuk <anton.mishchuk@gmail.com>
2022-03-31 20:18:22 +0200

    Set version to 0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Alf.MixProject do
   def project do
     [
       app: :alf,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
