beb32ed722607cbaf7f36df8ca71d997f2e9cd42
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2023-05-31 15:41:02 +0200

    Version 0.8.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Alf.MixProject do
   def project do
     [
       app: :alf,
-      version: "0.8.3",
+      version: "0.8.4",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: description(),
